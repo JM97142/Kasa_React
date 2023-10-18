5bc0fff61974b73108d40e6bed6bb5c5e7b8641b
JM97142 <jeanmichel.taret@gmail.com>
2023-10-18 08:08:24 +0200

    Ajout système du slideshow

--- src/components/Slideshow/Slighshow.jsx
+++ src/components/Slideshow/Slighshow.jsx
@@ -4,9 +4,20 @@ import '../Style/slideshow.css'
 function Slideshow(props) {
     const [slideshowId, setSlideshowId] = useState(0);
 
+    const previousImg = () => {
+        slideshowId > 0 ?
+            setSlideshowId(slideshowId - 1) : setSlideshowId(props.pictures.length - 1)
+    }
+    const nextImg = () => {
+        slideshowId < props.pictures.length - 1 ?
+            setSlideshowId(slideshowId + 1) : slideshowId(0)
+    }
+
     return (
         <div className="slideshow-wrapper">
+            <button onClick={() => previousImg()}>Prev</button>
             <img src={props.pictures[slideshowId]} alt="" className="slideshow-img" />
+            <button onClick={() => nextImg()}>Next</button>
         </div>
     )
 }
