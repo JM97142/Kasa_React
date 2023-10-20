d54ccca9796b7a3aed75e892f9c4bafc0d9fd608
JM97142 <jeanmichel.taret@gmail.com>
2023-10-20 07:40:36 +0200

    Media queries homepage

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -7,9 +7,10 @@ function Collapse(props) {
     const [isBtnClicked, setIsBtnClicked] = useState(false)
 
     return (
-        <div className='collapse-btn'>
-            <button onClick={() => setIsBtnClicked(!isBtnClicked)}>
+        <div className='collapse'>
+            <button className='collapse-btn' onClick={() => setIsBtnClicked(!isBtnClicked)}>
                 {props.title}
+                {/* <i class="fa-solid fa-chevron-up"></i> */}
                 <img src={Arrow} alt='' className={'collapse-arrow' + (isBtnClicked ? ' rotate' : '')} />
             </button>
             <p className={'collapse-btn-content' + (isBtnClicked ? ' isClicked' : '')}>{props.description}</p>
